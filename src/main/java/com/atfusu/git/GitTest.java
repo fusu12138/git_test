1f1268ec9441f0c62a48f70660da19cd9576533c
扶苏 <fusu12138@outlook.com>
2022-12-10 14:54:02 +0800

    第一次提交git

---
+++ src/main/java/com/atfusu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atfusu.git;
+
+/**
+ * Title:
+ * Author: fusu
+ * DateTime:  2022/12/10
+ * Description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("创建新的git文件");
+    }
+}
