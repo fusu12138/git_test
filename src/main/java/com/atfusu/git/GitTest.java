f023447ef48a4640685506a8506b6b08afc44afe
扶苏 <fusu12138@outlook.com>
2022-12-10 15:16:17 +0800

    dev在13行修改代码

--- src/main/java/com/atfusu/git/GitTest.java
+++ src/main/java/com/atfusu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("创建新的git文件");
         System.out.println("dev分支在修改代码");
+        System.out.println("dev在13行修改代码");
     }
 }
